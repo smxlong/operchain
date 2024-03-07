b549268461023eaa688b26bd2a8cfefc1bb4a6ad
Scott Long <smxlong@gmail.com>
2024-03-06 17:05:41 -0800

    check

--- chain.go
+++ chain.go
@@ -2,22 +2,30 @@ package operchain
 
 import (
 	"context"
+	"reflect"
 	"sync"
 	"time"
 
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/smxlong/operchain/internal/pcache"
 )
 
 // Chain is a chain of operchain Rules.
 type Chain struct {
+	client.Client
+
 	// Rules is the list of rules in the chain.
 	Rules []Rule
 	// Resources are the resources to load before running the chain.
 	Resources interface{}
 
+	// Reconciler state
 	lock     sync.Mutex
+	req      ctrl.Request
+	cache    *pcache.Cache
 	stop     bool
 	err      error
 	interval time.Duration
@@ -44,12 +52,16 @@ func Predicate(f func() bool) *predicate {
 }
 
 // Run runs an operchain.
-func (c *Chain) Run(ctx context.Context, cache *pcache.Cache) (ctrl.Result, error) {
+func (c *Chain) Run(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	c.stop = false
 	c.err = nil
 	c.interval = 0
+	c.cache = pcache.New()
+	if err := c.loadResources(ctx, req.NamespacedName); err != nil {
+		return ctrl.Result{}, err
+	}
 	for _, rule := range c.Rules {
-		if rule.When == nil || cache.Eval(rule.When) {
+		if rule.When == nil || c.cache.Eval(rule.When) {
 			rule.Do(ctx)
 			if c.stop || c.err != nil {
 				return ctrl.Result{Requeue: true, RequeueAfter: c.interval}, c.err
@@ -129,7 +141,7 @@ func Parallel(fns ...Action) Action {
 // actions in the subchain will be propagated to the parent chain.
 func (c *Chain) Subchain(sub *Chain) Action {
 	return func(ctx context.Context) {
-		result, err := sub.Run(ctx, nil)
+		result, err := sub.Run(ctx, c.req)
 		if err != nil {
 			c.doError(err)
 		}
@@ -140,7 +152,60 @@ func (c *Chain) Subchain(sub *Chain) Action {
 }
 
 // Initialize initializes a chain with the given resources and rules.
-func (c *Chain) InitializeChain(resources interface{}, rules []Rule) {
+func (c *Chain) InitializeChain(client client.Client, resources interface{}, rules []Rule) {
+	c.Client = client
 	c.Resources = resources
 	c.Rules = rules
+}
+
+// loadResources loads the resources for the chain.
+func (c *Chain) loadResources(ctx context.Context, name types.NamespacedName) error {
+	// The Resources should be a struct or pointer to a struct.
+	res := reflect.ValueOf(c.Resources)
+	if res.Kind() == reflect.Ptr {
+		res = res.Elem()
+	}
+	if res.Kind() != reflect.Struct {
+		panic("Resources must be a struct or pointer to a struct")
+	}
+	// Clear the resources to nil.
+	for i := 0; i < res.NumField(); i++ {
+		field := res.Field(i)
+		if field.CanSet() {
+			field.Set(reflect.Zero(field.Type()))
+		}
+	}
+	// Load the resources.
+	for i := 0; i < res.NumField(); i++ {
+		field := res.Field(i)
+		if !field.CanSet() {
+			continue
+		}
+		if err := c.loadResource(ctx, name, field); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// loadResource loads the resource for the given field.
+func (c *Chain) loadResource(ctx context.Context, name types.NamespacedName, field reflect.Value) error {
+	// The field should be a pointer to a struct.
+	if field.Kind() != reflect.Ptr {
+		panic("Resource fields must be pointers to structs")
+	}
+	field = field.Elem()
+	if field.Kind() != reflect.Struct {
+		panic("Resource fields must be pointers to structs")
+	}
+	// Load the resource.
+	obj := reflect.New(field.Type()).Interface().(client.Object)
+	if err := c.Get(ctx, name, obj); err != nil {
+		if client.IgnoreNotFound(err) != nil {
+			return err
+		}
+	} else {
+		field.Set(reflect.ValueOf(obj))
+	}
+	return nil
 }
